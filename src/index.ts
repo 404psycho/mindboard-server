c649a584d1fd20eeb2a87c5d3415220d168c20f5
404psycho <163523622+404psycho@users.noreply.github.com>
2024-03-29 12:40:48 +0500

    route added

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import connectToMongo from "./db";
 import groupRoutes from "./routes/GroupRoutes";
 import userRoutes from "./routes/UserRoutes";
 import boardRoutes from "./routes/BoardRoutes";
+import { Request, Response } from "express";
 env.config();
 
 const app = express();
@@ -26,6 +27,7 @@ app.use(bodyParser.json());
 app.use("/api/v1/user/", userRoutes);
 app.use("/api/v1/group/", groupRoutes);
 app.use("/api/v1/board/", boardRoutes);
+app.use("/", (req,res) => res.json({"message" : "Api Working"}))
 
 const server = http.createServer(app);
 
